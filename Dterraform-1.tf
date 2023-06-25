69b50ea758681ae8082b9b5b24af7691e2e8362d
iLimbani <134752486+iLimbani@users.noreply.github.com>
2023-06-25 18:21:57 -0400

    Update Dterraform-1.tf

--- Dterraform-1.tf
+++ Dterraform-1.tf
@@ -15,10 +15,40 @@ provider "aws" {
   region     = "us-east-2"
 }
 
+resource "aws_security_group_rule" "example1" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "sg-DockerVMSSH"
+}
+
+resource "aws_security_group_rule" "example2" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "sg-DockerVMHTTP"
+}
+
+resource "aws_security_group_rule" "example3" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "sg-DockerVMout"
+}
+
+
 resource "aws_instance" "myec2" {
    ami = "ami-05842f1afbf311a43"
    instance_type = "t2.micro"
- #  security_groups = [ ]
+   security_groups = [aws_security_group_rule.example1.id]
+   security_groups = [aws_security_group_rule.example2.id]
+   security_groups = [aws_security_group_rule.example3.id]
    key_name = "jenkins"
   tags = {
     Name = "Docker VM"
