4caf7eb3fb23aa5e2b0b84b85e49a5792248289d
iLimbani <134752486+iLimbani@users.noreply.github.com>
2023-06-30 16:52:44 -0400

    Update Dterraform-1.tf

--- Dterraform-1.tf
+++ Dterraform-1.tf
@@ -15,46 +15,17 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-resource "aws_security_group_rule" "example1" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-DockerVMSSH"
-}
-
-resource "aws_security_group_rule" "example2" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-DockerVMHTTP"
-}
-
-resource "aws_security_group_rule" "example3" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-DockerVMout"
-}
-
-
 resource "aws_instance" "myec2" {
    ami = "ami-05842f1afbf311a43"
    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allow_secid.id]
-//   security_groups = [aws_security_group_rule.example1.id]
-//   security_groups = [aws_security_group_rule.example2.id]
-  // security_groups = [aws_security_group_rule.example3.id]
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
    key_name = "jenkins"
   tags = {
     Name = "Docker VM"
   }
-provisioner "local-exec" {
-command="echo $(aws_instance.myec2.public_ip] >> /etc/ansible/hosts"
-}
+
+// provisioner "local-exec" {
+// command="echo $(aws_instance.myec2.public_ip] >> /etc/ansible/hosts"
+// }
+
 }
