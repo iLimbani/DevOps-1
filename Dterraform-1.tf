8ceab9997e47484874fe2e915066bbf6cbea301a
iLimbani <134752486+iLimbani@users.noreply.github.com>
2023-06-30 23:50:44 -0400

    Update Dterraform-1.tf

--- Dterraform-1.tf
+++ Dterraform-1.tf
@@ -26,26 +26,26 @@ key_name = "jenkins"
 }
 
 resource "aws_security_group" "allow_tls" {
-name = "terraform-sg"
-ingress {
-  description = " allow port 22 - inbound"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-ingress{
-description =" " allow port 80 - inbound"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  name = "terraform-sg"
+  ingress {
+    description = "allow port 22 - inbound"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-egress{
-description = " outbound"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress{
+    description = "allow port 80 - inbound"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress{
+    description = "outbound"
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
