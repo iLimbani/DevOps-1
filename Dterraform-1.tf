db42e1169511f0a0d305335832fe7b4c4ac2bea8
iLimbani <134752486+iLimbani@users.noreply.github.com>
2023-06-30 23:44:07 -0400

    Update Dterraform-1.tf

--- Dterraform-1.tf
+++ Dterraform-1.tf
@@ -18,8 +18,34 @@ provider "aws" {
 resource "aws_instance" "myec2" {
    ami = "ami-05842f1afbf311a43"
    instance_type = "t2.micro"
-   key_name = "jenkins"
+vpc_security_group_ids = [aws_security_group.allow_tls.id]  
+key_name = "jenkins"
   tags = {
     Name = "Docker VM"
   }
+}
+
+resource "aws_security_group" "allow_tls" {
+name = "terraform-sg"
+ingress {
+  description = " allow port 22 - inbound"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+ingress{
+description =" " allow port 80 - inbound"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+egress{
+description = " outbound"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
